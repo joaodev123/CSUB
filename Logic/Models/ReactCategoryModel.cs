032270adb7811b71c0db5008d57a7a342bbe33bc
roridev <t3ctotalmenterandom1@outlook.com>
2019-10-20 04:28:28 -0300

    Added Logic Layer for ReactRole System

--- Logic/Models/ReactCategoryModel.cs
+++ Logic/Models/ReactCategoryModel.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
+
 namespace Logic.Models
 {
     public class ReactCategoryModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int ChannelId {get;set;}
         public string Description { get; set; }
         public List<int> Roles { get; set; }
     }
--- Logic/Models/ReactRoleModel.cs
+++ Logic/Models/ReactRoleModel.cs
@@ -3,6 +3,7 @@ namespace Logic.Models
     public class ReactRoleModel
     {
         public int Id { get; set; }
+        public int CategoryId { get; set; }
         public ulong EmojiId { get; set; }
         public ulong RoleId { get; set; }
     }
